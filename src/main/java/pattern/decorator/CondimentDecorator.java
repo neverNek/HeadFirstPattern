f60a6ba34c1a094f65fdc4d3c99a29dbbb96b2e7
neverNek <axwqaz@gmail.com>
2022-07-10 23:18:32 +0500

    Паттерн Декоратор

---
+++ src/main/java/pattern/decorator/CondimentDecorator.java
@@ -0,0 +1,11 @@
+package pattern.decorator;
+
+public abstract class CondimentDecorator extends Beverage {
+    Beverage beverage;
+
+    public abstract String getDescription();
+
+    public Size getSize() {
+        return beverage.getSize();
+    }
+}
---
+++ src/main/java/pattern/decorator/DarkRoast.java
@@ -0,0 +1,13 @@
+package pattern.decorator;
+
+public class DarkRoast extends Beverage{
+
+    public DarkRoast() {
+        description = "DarkRoast Coffee";
+    }
+
+    @Override
+    public double cost() {
+        return .99;
+    }
+}
